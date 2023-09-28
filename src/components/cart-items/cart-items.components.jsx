d674731203bc43ef8b0c3d60e112c9fca35ae3b9
ise2005best <93008391+ise2005best@users.noreply.github.com>
2023-09-28 15:51:11 +0100

    add to cart completed

--- src/components/cart-items/cart-items.components.jsx
+++ src/components/cart-items/cart-items.components.jsx
@@ -2,15 +2,18 @@ import './cart-items.styles.scss';
 const CartItem = ({CartItem}) =>{
     const {title, quantity, price, imageUrl} = CartItem;
     return(
-        <div>
+        <div className='cart-item-container'>
             <img src={imageUrl} alt={`${title}`} style={{height: 80, width: 80}}/>
             <br/>
+            <div className='item-details'>
             <span>
                 {title}
             </span>
             <p>
                 {quantity} x {price}
             </p>
+            </div>
+           
         </div>
     )
 }
